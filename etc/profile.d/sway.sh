464aad7491252e2ae21c7559f779435e9ba561e3
maoyikun <maoyikun@jzsz.cc>
2024-03-23 00:50:17 +0800

    feat: update some config

--- etc/profile.d/sway.sh
+++ etc/profile.d/sway.sh
@@ -1,7 +1,7 @@
 # sway 相关配置
 export QT_QPA_PLATFORM=wayland
 # 移除qt应用窗口装饰
-export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 # 处理java程序有时不能启动的问题
---
+++ from_local_machine.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 将本地的相关配置文件同步到本仓库
+
+
+# 同步 .zshrc 配置
+echo "同步 $HOME/.zshrc 内容到 .zshrc"
+cat ~/.zshrc > ./.zshrc
+
+# 同步 sway 配置文件
+echo "同步 $HOME/.config/sway 内容到 .config/sway"
+cp -R $HOME/.config/sway/ ./.config/
+
+# 同步 waybar 配置文件
+echo "同步 $HOME/.config/waybar 内容到 .config/waybar"
+cp -R $HOME/.config/waybar/ ./.config/
+
+# 同步 foot 配置文件
+echo "同步 $HOME/.config/foot 内容到 .config/foot"
+cp -R $HOME/.config/foot/ ./.config/
+
+# 同步 fontconfig 配置文件
+echo "同步 $HOME/.config/fontconfig 内容到 .config/fontconfig"
+cp -R $HOME/.config/fontconfig/ ./.config/
+
+
+# 同步 /etc/pam.d 配置文件
+# set $pamd_path etc/pam.d
+
+# echo "同步$pamd_path目录的文件需要切换到root用户"
+# set timeout 1
+# spawn su root
+
+# echo "同步 /$pamd_path/login 内容到 $pamd_path/login"
+# cat /$pamd_path/login > $pamd_path/login
